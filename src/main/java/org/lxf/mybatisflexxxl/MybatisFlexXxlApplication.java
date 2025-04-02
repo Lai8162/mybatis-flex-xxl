177ae7b20bb1baca8ced5cfb325f14c9386b7ada
lxf <694023262@qq.com>
2025-04-03 01:30:00 +0800

    feat：init初始化

---
+++ src/main/java/org/lxf/mybatisflexxxl/MybatisFlexXxlApplication.java
@@ -0,0 +1,13 @@
+package org.lxf.mybatisflexxxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisFlexXxlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisFlexXxlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lxf/mybatisflexxxl/MybatisFlexXxlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lxf.mybatisflexxxl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisFlexXxlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
